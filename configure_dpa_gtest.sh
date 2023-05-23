96397a802e61b88b948c29696c063444c6936ced
Alex Mikheev <amikheev@nvidia.com>
2023-05-23 08:20:41 +0000

    dpa: dpa-clang cross compilation support
    The commit adds support for the dpa-clang build
    on BF3.
    
    Flexio still must be used as a separate installed
    bundle and not as a subproject.
    
    Picolibc is used as libc and is built in a subproject
    and is not taken from the flexio.
    
    The reasons are:
    - we want to use full libc headers to make porting easier
    - we want to use optimized memcpy
    
    Signed-off-by: Alex Mikheev <amikheev@nvidia.com>

--- configure_dpa_gtest.sh
+++ configure_dpa_gtest.sh
@@ -8,8 +8,18 @@
 #      -Dwith-flexio=/labhome/amikheev/workspace/NVME/apu/flexio-sdk-new/inst_feb2022 \
 #      $BUILD_DIR/meson_build_cross
 
+#
+# On x86 gcc
+CROSS_FILE=cross-gcc-riscv64.txt
+FLEXIO=/labhome/amikheev/workspace/NVME/apu/flexio-sdk-jan2023/inst
+
+#
+# dpa-clang on arm
+CROSS_FILE=cross-dpa-clang-riscv64.txt
+FLEXIO=/labhome/amikheev/workspace/NVME/apu/flexio-sdk-jan2023/inst_arm
+
 meson setup \
-      --cross-file cross-gcc-riscv64.txt \
+      --cross-file $CROSS_FILE \
       -Denable-gtest=true \
-      -Dwith-flexio=/labhome/amikheev/workspace/NVME/apu/flexio-sdk-jan2023/inst \
+      -Dwith-flexio=$FLEXIO \
       $*
